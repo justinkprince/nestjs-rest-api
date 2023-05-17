240b742deba90d36c72b4b94d92581d9c091ae90
Justin Prince <justin.prince@disneystreaming.com>
2023-05-16 23:01:34 -0400

    More stuff

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import * as showdown from 'showdown';
+import { readFile } from 'fs/promises';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello(): Promise<string> {
+    const markdown = await readFile('./README.md', 'utf-8');
+    const converter = new showdown.Converter();
+
+    return converter.makeHtml(markdown);
   }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -19,7 +19,7 @@ export const extractParams = (params: any): QueryParams => {
   };
 };
 
-export function randomEnum<T>(anEnum: T): T[keyof T] {
+export const randomEnum = <T>(anEnum: T): T[keyof T] => {
   const enumValues = Object.values(anEnum) as unknown as T[keyof T][];
   const randomIndex = Math.floor(Math.random() * enumValues.length);
 
