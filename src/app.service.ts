6273f8bf5e7ed26377f75dfb87bcd3838d55d757
Justin Prince <justin.prince@disneystreaming.com>
2023-05-16 23:09:29 -0400

    More stuff

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import * as os from 'os';
 import * as showdown from 'showdown';
 import { readFile } from 'fs/promises';
 
 @Injectable()
 export class AppService {
   async getHello(): Promise<string> {
-    const markdown = await readFile('./README.md', 'utf-8');
+    const rawMarkdown = await readFile('./README.md', 'utf-8');
+    const markdown = rawMarkdown.replace(/http:\/\/localhost:3000/, os.hostname());
     const converter = new showdown.Converter();
 
     return converter.makeHtml(markdown);
